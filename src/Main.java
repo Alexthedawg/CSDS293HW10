984b9bbaff530ce43c3dae358acdeababa953b79
Alex Wang <alex-wang@DESKTOP-E2KC4VG>
2022-11-18 01:11:56 -0500

    added Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,188 @@
 import java.io.*;
 import java.nio.CharBuffer;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+/**
+ * A class that parses user input to generate a blacklist of
+ * Hosts that surpassed a specified threshold for failed logins
+ * and illegal user access. A blacklist is reported to the user
+ * via standard output.
+ */
 public final class Main {
-  
-  private static final int DEFAULT_THRESHOLD = 3;
-  
-  private final HashMap<String, String> denyList = new HashMap<>();
-  
-  public static void main(String[] args) {
-    
-    /* Setting the security threshold. */
-    final int threshold = setSecurityThreshold(args);
-    
-    /* Reading security log input from standard input. */
-    final String input = getSecurityLogInput();
-    
-    System.out.print(input);
-  }
-  
-  /**
-   * A method to set the security threshold. If no argument is
-   * provided, then the threshold is set to the default 3.
-   * @param args a String array that may contain a threshold value
-   * @return the threshold value obtained from args or by default
-   */
-  public static int setSecurityThreshold(final String[] args) {
-    /* Null check. */
-    Objects.requireNonNull(args);
-    
-    /* 
-     * If there is an argument provided, then the threshold is
-     * the first argument provided in the array, regardless of
-     * any other arguments provided in the array. If there is no
-     * argument provided, then the security threshold is set to 
-     * DEFAULT_THRESHOLD.
+
+    private static final int DEFAULT_THRESHOLD = 3;
+    private static final Pattern FAILED_LOGINS_PATTERN = Pattern.compile("Failed *logins *from:\\s*", Pattern.CASE_INSENSITIVE);
+    private static final Pattern ILLEGAL_USERS_PATTERN = Pattern.compile("Failed *logins *from:\\s*", Pattern.CASE_INSENSITIVE);
+    private static final Pattern IP_ADDRESS_PATTERN = Pattern.compile("\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}");
+    private static final Pattern DOMAIN_NAME_PATTERN = Pattern.compile("(?<=\\()[a-zA-Z0-9-.]{2,}(?=\\))");
+    private static final Pattern TRIES_NUMBER_PATTERN = Pattern.compile("\\d+");
+    private static final int CHAR_LIM = 80;
+    private static final String ENDLINE = "\\\n";
+    private static final int ENDLINELEN = ENDLINE.length();
+    private static final String NEWLINE = "     ";
+    private static final String SEPARATOR = ", ";
+
+    /**
+     * Runs this program.
+     * @param args the threshold for the run
      */
-    final int threshold = (args.length > 0)
-      ? Integer.parseInt(args[0])
-      : DEFAULT_THRESHOLD;
-    
-    return threshold;
-  }
-  
-  //TODO comment all this code.
-  public static String getSecurityLogInput() {
-    StringBuilder sb = new StringBuilder();
-    
-    System.out.println("Enter log summary below (press ctrl+d when finished):");
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-      CharBuffer buffer = CharBuffer.allocate(1024);
-      
-      while (reader.read(buffer) != -1) {
-        buffer.flip();
-        sb.append(buffer);
-        buffer.clear();
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    public static void main(String[] args) {
+
+        /* Setting the security threshold. */
+        final int threshold = setSecurityThreshold(args);
+
+        /* Reading security log input from standard input. */
+        final String input = getSecurityLogInput();
+
+        /* Initializing the denied list for the ip addresses / domains. */
+        HashMap<Host, Integer> list = new HashMap<>();
+
+        /* Collect the Host Info of failed logins and illegal users. */
+        list = collectHostInfo(Main.FAILED_LOGINS_PATTERN, input, list);
+        list = collectHostInfo(Main.ILLEGAL_USERS_PATTERN, input, list);
+
+        /* Report Host Info to standard output. */
+        String denyList = reportInfo(list, threshold);
+        System.out.println(denyList);
     }
-    return sb.toString();
-  }
+
+    /**
+     * A method to set the security threshold. If no argument is
+     * provided, then the threshold is set to the default 3. The
+     * threshold may not be set to 0 or less.
+     * @param args a String array that may contain a threshold value
+     * @return the threshold value obtained from args or by default
+     * @throws IllegalArgumentException if the threshold is set <= 0
+     */
+    public static int setSecurityThreshold(final String[] args) {
+        /* Null check. */
+        Objects.requireNonNull(args);
+
+        /*
+         * If there is an argument provided, then the threshold is
+         * the first argument provided in the array, regardless of
+         * any other arguments provided in the array. If there is no
+         * argument provided, then the security threshold is set to
+         * DEFAULT_THRESHOLD.
+         */
+        final int threshold = (args.length > 0)
+                ? Integer.parseInt(args[0])
+                : DEFAULT_THRESHOLD;
+
+        if (threshold <= 0) throw new IllegalArgumentException("Threshold cannot be set to this value.");
+
+        return threshold;
+    }
+
+    /**
+     * A method to get input from command line and store it
+     * in a String.
+     * @return a String containing input from the command line
+     */
+    public static String getSecurityLogInput() {
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("Enter log summary below (press ctrl+d when finished):");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            CharBuffer buffer = CharBuffer.allocate(1024);
+
+            while (reader.read(buffer) != -1) {
+                buffer.flip();
+                sb.append(buffer);
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * A method to add Host information to a list that tracks the amount of tries.
+     * The method adds Host information from a specified Pattern in the input
+     * until it reaches a black line.
+     *
+     * @param pattern the Pattern of a section to search under
+     * @param input   the user input to scan
+     * @param list    the list to add Host info to
+     * @return the list with the updated Hosts found
+     */
+    public static HashMap<Host, Integer> collectHostInfo(Pattern pattern, String input, HashMap<Host, Integer> list) {
+        Scanner scanner = new Scanner(input);
+
+        /* Check that the Pattern exists in the input given. */
+        if (scanner.findWithinHorizon(pattern, 0) != null) {
+            /* Run while the next line exists and is not blank. */
+            do {
+                String line = scanner.nextLine();
+                if (line.length() == 0) { break; }
+                Host host = getHost(line);
+                /* Add host to list. */
+                list.put(host, list.getOrDefault(host, 0) + host.tries());
+            } while (scanner.hasNextLine());
+        }
+        scanner.close();
+        return list;
+    }
+
+    /**
+     * A method to get a Host from a formatted String of data.
+     * @param line a String containing data to create a Host
+     * @return a new Host based on the data of the line
+     */
+    public static Host getHost(String line) {
+        Objects.requireNonNull(line);
+
+        /* Initializing variables. */
+        String ipAddress, domainName = null, tries;
+
+        /* Getting ip address. */
+        Scanner scanner = new Scanner(line);
+        ipAddress = scanner.findWithinHorizon(Main.IP_ADDRESS_PATTERN, 0);
+
+        /* Getting domain name (if it exists). */
+        domainName = scanner.findWithinHorizon(Main.DOMAIN_NAME_PATTERN, 0);
+
+        /* Getting number of tries. */
+        tries = scanner.findWithinHorizon(Main.TRIES_NUMBER_PATTERN, 0);
+        scanner.close();
+
+        /* Make sure that a valid Host can be created from the info. */
+        Objects.requireNonNull(ipAddress);
+        Objects.requireNonNull(tries);
+
+        return new Host(ipAddress, domainName, Integer.parseInt(tries));
+    }
+
+    /**
+     * A method to report the data in the blacklisted Host list
+     * to standard output.
+     *
+     * @param list      the list with the given Host Info
+     * @param threshold the threshold for blacklisting Hosts.
+     * @return
+     */
+    public static String reportInfo(HashMap<Host, Integer> list, int threshold) {
+        String hosts = list.keySet().stream()
+                .filter(host -> host.tries() >= threshold)
+                .map(host -> host.toString())
+                .collect(Collectors.joining("\n"));
+
+        Scanner scanner = new Scanner(hosts);
+        StringBuilder sb = new StringBuilder("     ");
+
+        while (scanner.hasNext()) {
+            String host = scanner.next();
+            if (host.length() + sb.length() < CHAR_LIM - 1) {
+                sb.append(host + SEPARATOR);
+            } else {
+                sb.append(ENDLINE + NEWLINE + host + SEPARATOR);
+            }
+        }
+        scanner.close();
+        sb.delete(sb.length() - ENDLINELEN, sb.length());
+        return sb.toString();
+    }
 }
--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,8 +1,85 @@
-import org.junit.*;
+mport org.junit.Test;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 public class MainTest {
-  
-  
+
+    private static final int DEFAULT_THRESHOLD = 3;
+    private static final Pattern FAILED_LOGINS_PATTERN = Pattern.compile("Failed *logins *from:\\s*", Pattern.CASE_INSENSITIVE);
+    private static final Pattern ILLEGAL_USERS_PATTERN = Pattern.compile("Failed *logins *from:\\s*", Pattern.CASE_INSENSITIVE);
+
+    @Test
+    public void testSetSecurityThreshold() {
+        int threshold = -1;
+        String[] args;
+
+        /* Normal Case */
+        args = new String[]{"5"};
+        threshold = Main.setSecurityThreshold(args);
+        assertEquals(5, threshold);
+
+        /* args.length > 1 */
+        args = new String[]{"4", "2", "10"};
+        threshold = Main.setSecurityThreshold(args);
+        assertEquals(4, threshold);
+
+        args = new String[]{"7", "hi", "m, ad,f nas, n"};
+        threshold = Main.setSecurityThreshold(args);
+        assertEquals(7, threshold);
+
+        /* args.length == 0 */
+        args = new String[0];
+        threshold = Main.setSecurityThreshold(args);
+        assertEquals(DEFAULT_THRESHOLD, threshold);
+
+        /* args[0] <= 0 */
+        assertThrows(IllegalArgumentException.class, () -> Main.setSecurityThreshold(new String[]{"0", "2345"}));
+        assertThrows(IllegalArgumentException.class, () -> Main.setSecurityThreshold(new String[]{"-234", "0"}));
+        assertThrows(IllegalArgumentException.class, () -> Main.setSecurityThreshold(new String[]{"-234", "0"}));
+
+        /* args[0] is not an integer */
+        assertThrows(NumberFormatException.class, () -> Main.setSecurityThreshold(new String[]{"hi", "3"}));
+        assertThrows(NumberFormatException.class, () -> Main.setSecurityThreshold(new String[]{" asd", "3"}));
+        assertThrows(NumberFormatException.class, () -> Main.setSecurityThreshold(new String[]{"-5.6", "3"}));
+        assertThrows(NumberFormatException.class, () -> Main.setSecurityThreshold(new String[]{"5.6", "3"}));
+    }
+
+    @Test
+    public void testGetHost() {
+        Host expected;
+        String line;
+
+        /* Normal Case */
+        expected = new Host("2.115.68.148", "host148-68-static.115-2-b.business.telecomitalia.it", 7);
+        line = "2.115.68.148 (host148-68-static.115-2-b.business.telecomitalia.it): 7 times";
+        assertEquals(expected, Main.getHost(line));
+
+        /* domainName == null */
+        expected = new Host("27.254.44.45", null, 1);
+        line = "27.254.44.45: 1 time";
+        assertEquals(expected, Main.getHost(line));
+    }
+
+    @Test
+    public void testGetSecurityLogInput() {
+        /* I have no idea how to test this. */
+    }
+
+    @Test
+    public void testReportInfo() {
+        String input = "Failed logins from: \\n    2.115.68.148 (host148-68-static.115-2-b.business.telecomitalia.it): 7 times \\n    27.254.44.45: 6 times \\n    31.186.13.221 (server-1.gencfb.org): 7 times \\n    43.255.188.161: 2968 times \\n    50.62.42.229 (ip-50-62-42-229.ip.secureserver.net): 7 times \\n    50.63.56.230 (ip-50-63-56-230.ip.secureserver.net): 7 times \\n    58.137.72.110: 7 times \\n    59.175.153.96 (96.153.175.59.broad.wh.hb.dynamic.163data.com.cn): 7 times \\n    63.251.162.66 (insynq-1.border2.sef003.pnap.net): 396 times\\n\\nIllegal users from: \\n    2.115.68.148 (host148-68-static.115-2-b.business.telecomitalia.it): 1 time \\n    27.254.44.45: 1 time \\n    31.186.13.221 (server-1.gencfb.org): 1 time \\n    50.62.42.229 (ip-50-62-42-229.ip.secureserver.net): 1 time \\n    50.63.56.230 (ip-50-63-56-230.ip.secureserver.net): 1 time \\n    58.137.72.110: 1 time \\n    59.175.153.96 (96.153.175.59.broad.wh.hb.dynamic.163data.com.cn): 1 time \\n    63.251.162.66 (insynq-1.border2.sef003.pnap.net): 521 times";
+        String expected = "     server-1.gencfb.org, 43.255.188.161, \\\\n     96.153.175.59.broad.wh.hb.dynamic.163data.com.cn, \\\\n     ip-50-63-56-230.ip.secureserver.net, \\\\n     insynq-1.border2.sef003.pnap.net, \\\\n     58.137.72.110, \\\\n     host148-68-static.115-2-b.business.telecomitalia.it, \\\\n     ip-50-62-42-229.ip.secureserver.net, \\\\n     27.254.44.45";
+        HashMap list = new HashMap();
+
+        list = Main.collectHostInfo(FAILED_LOGINS_PATTERN, input, list);
+        list = Main.collectHostInfo(ILLEGAL_USERS_PATTERN, input, list);
+
+        String actual = Main.reportInfo(list, DEFAULT_THRESHOLD);
+
+        /* Normal Case */
+    }
 }
+
